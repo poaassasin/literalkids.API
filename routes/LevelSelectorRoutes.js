d2d8ebada758788057c8f8aafdfa6e61477082dc
poaassasin <109660255+poaassasin@users.noreply.github.com>
2025-06-20 19:35:45 +0700

    Update LevelSelectorRoutes.js

--- routes/LevelSelectorRoutes.js
+++ routes/LevelSelectorRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db_local');
+const db = require('../config/database.js');
 
 // GET /api/levelselector/school-levels
 router.get('/school-levels', (req, res) => {
