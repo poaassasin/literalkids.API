ec9cc5c044e298e62f00caa7639d4535adeb8337
poaassasin <109660255+poaassasin@users.noreply.github.com>
2025-06-20 19:20:06 +0700

    Update db_railway.js

--- config/db_railway.js
+++ config/db_railway.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSERNAME,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 db.connect((err) => {
