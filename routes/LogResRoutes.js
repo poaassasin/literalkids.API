8978915500a4ebe07b521ad9bd75f20831b21351
poaassasin <109660255+poaassasin@users.noreply.github.com>
2025-06-20 19:35:58 +0700

    Update LogResRoutes.js

--- routes/LogResRoutes.js
+++ routes/LogResRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const db = require('../config/db_local');
+const db = require('../config/database.js');
 
 
 router.post("/register", async (req, res) => {
