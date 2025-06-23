2ede4fafeb09467846d037969a79c5b374ff5014
poaassasin <109660255+poaassasin@users.noreply.github.com>
2025-06-23 20:01:38 +0700

    Update LogResController.js

--- controllers/LogResController.js
+++ controllers/LogResController.js
@@ -22,17 +22,17 @@ const loginUser = (req, res) => {
 
     const sql = "SELECT * FROM users WHERE email = ?";
     db.query(sql, [email], async (err, results) => {
-        if (err) return res.status(500).json({ message: "Error logging in", error: err });
+        if (err) return res.status(500).json({ error: "Error logging in" });
 
         if (results.length === 0) {
-            return res.status(401).json({ message: "Invalid email or password" });
+            return res.status(401).json({ error: "Invalid email or password" });
         }
 
         const user = results[0];
         const isMatch = await bcrypt.compare(password, user.password);
 
         if (!isMatch) {
-            return res.status(401).json({ message: "Invalid email or password" });
+            return res.status(401).json({ error: "Invalid email or password" });
         }
 
         const token = jwt.sign({ id: user.id, email: user.email },
